202e1cfde5807dbffbaa2acf23284b313118615b
mankamolnar <markmanomolnar@gmail.com>
2016-10-17 15:12:59 +0200

    i hope now it gonna work :D

---
+++ 100doors-02.py
@@ -0,0 +1,25 @@
+#write out which doors are open
+def writeOpenedDoors(allDoors):
+    string = "Opened doors: "
+    for i in range(0, len(allDoors)):
+        if allDoors[i] == "o":
+            string += str(i)+", "
+    print(string)
+
+#init
+doors = ["c"] * 100
+
+#magic :D
+for i in range(1, 101):
+    j = 0
+    writeOpenedDoors(doors)
+    for value in doors:
+        if (j % i == 0):
+            if doors[j] == "c":
+                doors[j] = "o"
+            elif doors[j] == "o":
+                doors[j] = "c"
+        j += 1
+
+#write out the opened doors
+writeOpenedDoors(doors)
